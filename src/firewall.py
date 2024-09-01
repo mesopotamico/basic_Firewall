e7eea92a34d31681b6a139685d69e09bad8cce80
Nicolas Duque <n.duque1@utp.edu.co>
2024-09-01 18:01:19 -0500

    Core of Firewall Class

--- src/firewall.py
+++ src/firewall.py
@@ -1,4 +1,23 @@
 # src/firewall.py
+class Firewall:
+    def __init__(self):
+        self.first_handler = None
+    
+    def set_first_handler(self, handler):
+        self.first_handler = handler
+
+    def process(self, packet):
+        if self.first_handler:
+            return self.first_handler.handle_request(packet)
+        else:
+            print("No handlers.")
+            return "No processing"
+
+
+
+
+
+
 def main():
     print("Starting the firewall...")
     # Firewall code goes here
