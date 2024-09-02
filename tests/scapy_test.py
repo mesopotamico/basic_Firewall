87a70e93f28b6299bd743780ebff2bfecaa5144b
Nicolas Duque Gutierrez <n.duque1@utp.edu.co>
2024-09-02 14:21:57 -0500

    Reach the data of every packet

--- tests/scapy_test.py
+++ tests/scapy_test.py
@@ -1,4 +1,14 @@
 from scapy.all import *
+# Definir una función de callback para procesar los paquetes
+def procesar_paquete(paquete):
+    print("Paquete capturado:")
+    ip = paquete.getlayer(IP)
+    print(f"La ip del paquete es: {ip.src}")
+    # También puedes acceder a atributos específicos, por ejemplo:
+    # print(paquete[IP].src)  # Dirección IP de origen
+
+# Capturar paquetes en tiempo real
+sniff(prn=procesar_paquete, count=2)  # Captura 10 paquetes
 
 ip = IP(dst="www.google.com")
 
@@ -7,8 +17,8 @@ tcp = TCP(dport=80)
 packet = ip/tcp
 
 #`packet.show()
-packets = sniff(count = 5)
+#packets = sniff(count = 5)
 
-packets.summary()
+#packets.summary()
 
-send(packet)
+#send(packet)
