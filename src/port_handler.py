c2db7cae937088954caf9d50dcd5891bf0f32193
Nicolas Duque Gutierrez <n.duque1@utp.edu.co>
2024-09-05 17:34:37 -0500

    TCP port filter

--- src/port_handler.py
+++ src/port_handler.py
@@ -15,12 +15,22 @@ class PortFilterHandler(Handler):
         if packet.haslayer(TCP):
             tcp = packet.getlayer(TCP)
             if self.is_allowed_port(tcp.sport):
-                print(f"The scanned port {tcp.sport} is avaliable")
+                print(f"The scanned TCP port {tcp.sport} is avaliable")
                 if self.next_handler:
                     return self.next_handler.handler_request(packet) 
             else:
-                print(f"Packet from {ip.src} with the port {tcp.sport} blocked by filter.")
+                print(f"TCP Packet from {ip.src} with the port {tcp.sport} blocked by filter.")
                 return "Blocked by TCP filter."
+
+        elif packet.haslayer(UDP):
+            udp = packet.getlayer(UDP)
+            if self.is_allowed_port(udp.sport):
+                print(f"The scanned UDP port {udp.sport} is avaliable")
+                if self.next_handler:
+                    return self.next_handler.handler_request(packet)
+            else:
+                print(f"UDP Packet from {ip.src} with the port {udp.sport} blocked by filter.")
+                return "Blocked by UDP filter."
 
     def is_allowed_port(self, port):
         allowed_ports = [80, 443]
