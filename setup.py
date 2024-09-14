d9099c8028d00b92b396a5bcbfe1bca0d7020fa4
Nicolas Duque Gutierrez <n.duque1@utp.edu.co>
2024-09-14 14:48:20 -0500

    Install method

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='firewall_project',  # Name of your project
-    version='0.1',            # Version of your project
-    packages=find_packages(where="src"),  # Automatically find all packages in src
-    package_dir={"": "src"},  # Specify that packages are found in src
-    install_requires=[        # Dependencies
+    name='firewall_project',
+    version='0.1',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
         'scapy',
+        'requests',
+        'altgraph==0.17.2',
+        'future==0.18.2',
+        'macholib==1.15.2',
+        'six==1.15.0',
+        'wheel==0.44.0',
     ],
-    entry_points={            # Define the entry point for the command-line tool
+    entry_points={
         'console_scripts': [
-            'firewall=firewall:main',  # Format: 'command=module:function'
+            'firewall=firewall:main',
         ],
     },
+    description='A basic firewall project using scapy',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Nicolas Duque',
+    author_email='n.duque1@utp.edu.co',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
-
